3967564f623ca1efe45b1f57a913180de0f39702
Duvan Pedraza <dpedraza9@gmail.com>
2025-04-18 21:05:29 -0500

    Se agrega el Project Carpet 'INTEGRADOR4A' en el cual esta la informacion con el codigo de java y los recursos utilizados para la interfaz del proyecto, se agrega nuevamente la carpeta de la documentacion que se borro accidentalmente, pero se guarda en docs y se crea dentro de esta misma la carpeta planteamiento problema moviendo el documento de la propuesta proyecto integrador

---
+++ INTEGRADOR4A/src/main/java/com/mycompany/integrador4a/INTEGRADOR4A.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.integrador4a;
+
+import com.mycompany.integrador4a.igu.Login;
+
+/**
+ *
+ * @author Santiago
+ */
+public class INTEGRADOR4A {
+
+    public static void main(String[] args) {
+        Login iniciar = new Login();
+        iniciar.setVisible(true);
+    }
+}
